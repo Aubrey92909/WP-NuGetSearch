0190f02ec45b1f002b6b05e38eedcf5196cd0b6c
Edi Wang <Edi_Wang@outlook.com>
2014-06-24 15:47:17 +0800

    remove http client lib

---
+++ NuGetSearch/NuGetApiClientLib/NuGetApiClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services.Client;
+using System.Linq;
+using System.Threading.Tasks;
+using NuGetApiClientLib.NuGetService;
+
+namespace NuGetApiClientLib
+{
+    public class NuGetApiClient
+    {
+        private V2FeedContext context;
+
+        #region Properties
+
+        public string ApiBaseAddress { get; set; }
+
+        public string SearchTerm { get; set; }
+
+        public int PageSize { get; set; }
+
+        public bool IsIncludePreRelease { get; set; }
+
+        #endregion
+
+        public NuGetApiClient(string apiBaseAddress)
+        {
+            ApiBaseAddress = apiBaseAddress;
+            SearchTerm = string.Empty;
+            context = new V2FeedContext(new Uri(ApiBaseAddress));
+        }
+
+        public NuGetApiClient SetPageSize(int pageSize)
+        {
+            PageSize = pageSize;
+            return this;
+        }
+
+        public NuGetApiClient SearchFor(string keyword)
+        {
+            SearchTerm = keyword;
+            return this;
+        }
+
+        public NuGetApiClient IncludePreRelease(bool includePreRelease)
+        {
+            IsIncludePreRelease = includePreRelease;
+            return this;
+        }
+
+
+        public async Task<Response<List<V2FeedPackage>>> GetDataAsync(string searchTerm, int pageIndex)
+        {
+            try
+            {
+                IDictionary<string, object> queryOptions = new Dictionary<string, object> {
+                    { "filter", "IsLatestVersion"},
+                    { "searchTerm", "'" + UrlEncodeOdataParameter(searchTerm) + "'"},
+                    { "includePrerelease","false"}
+                };
+
+                int startRow = (pageIndex - 1) * PageSize;
+                var nquery = context.CreateQuery<V2FeedPackage>("Search")
+                                    .OrderByDescending(p => p.DownloadCount)
+                                    .Skip(startRow)
+                                    .Take(PageSize);
+
+                var query = (DataServiceQuery<V2FeedPackage>)nquery;
+                query = queryOptions.Aggregate(query, (current, pair) => current.AddQueryOption(pair.Key, pair.Value));
+                var taskFactory = new TaskFactory<IEnumerable<V2FeedPackage>>();
+                var result = await taskFactory.FromAsync(query.BeginExecute(null, null), query.EndExecute);
+                var packages = result.ToList();
+
+                return new Response<List<V2FeedPackage>>()
+                {
+                    IsSuccess = true,
+                    Item = packages
+                };
+            }
+            catch (Exception e)
+            {
+                return new Response<List<V2FeedPackage>>()
+                {
+                    Message = e.Message
+                };
+            }
+        }
+
+        private static string UrlEncodeOdataParameter(string value)
+        {
+            if (!String.IsNullOrEmpty(value))
+            {
+                // OData requires that a single quote MUST be escaped as 2 single quotes.
+                // In .NET 4.5, Uri.EscapeDataString() escapes single quote as %27. Thus we must replace %27 with 2 single quotes.
+                // In .NET 4.0, Uri.EscapeDataString() doesn't escape single quote. Thus we must replace it with 2 single quotes.
+                return Uri.EscapeDataString(value).Replace("'", "''").Replace("%27", "''");
+            }
+
+            return value;
+        }
+    }
+}
